65e3c96b9a5130b13b1de7a74da5ad7f53e7923b
lichangwei <lichangwei@outlook.com>
2024-12-27 20:20:36 +0800

    day05

---
+++ 05/t1.ts
@@ -0,0 +1,28 @@
+const text = await Deno.readTextFile("./05/input.txt");
+
+const [section1, section2] = text.split("\n\n");
+
+const before = section1
+  .split("\n")
+  .map((line) => line.split("|"))
+  .reduce((map, [left, right]) => {
+    map[right] = map[right] || [];
+    map[right].push(left);
+    return map;
+  }, {});
+
+const result = section2
+  .split("\n")
+  .map((line) => line.split(","))
+  .filter((array) => {
+    return array.every((item, index, array) => {
+      if (before[item]) {
+        return before[item].every((item) => array.indexOf(item) < index);
+      }
+      return true;
+    });
+  })
+  .map((array) => array[(array.length - 1) / 2])
+  .reduce((sum, item) => sum + parseInt(item, 10), 0);
+
+  console.log(result);
